0ad5c14df1524aee328721747fc133e4e915b5ba
Djordje Vukovic <djordje@gyorgy.io>
2024-04-17 15:44:55 +0200

    Initial implementation

--- packages/slider/index.tsx
+++ packages/slider/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import styles from './styles.module.css';
 import classnames from 'classnames';
+import { useSlide } from './useSlide';
 
 export interface Props {
   className?: string;
@@ -9,6 +10,7 @@ export interface Props {
   max?: number;
   min?: number;
   style: React.CSSProperties;
+  onChange?: (value: number, oldValue: number) => void;
 }
 
 /**
@@ -16,15 +18,17 @@ export interface Props {
  * @param props
  */
 const Slider: React.FC<Props> = (props) => {
-  const { className, defaultValue = 0, max = 100, min = 0, style } = props;
+  const { className, defaultValue = 0, max = 100, min = 0, onChange, style } = props;
 
   // Hooks
   const [value, setValue] = React.useState(defaultValue || min);
+  const draggingArea = React.useRef<HTMLDivElement>(null);
 
   // Setup
   const progress = (value - min) / (max - min);
   const percentage = `${progress * 100}%`;
   const handleLeft = `calc(${percentage} - 12px)`;
+  const config = { draggingArea, max, min, value };
 
   // Styles
   const classes = classnames(styles.component, className);
@@ -32,6 +36,10 @@ const Slider: React.FC<Props> = (props) => {
   const classesForeground = classnames(styles.bar, styles.foreground);
 
   // Handlers
+  const onDragUpdate = (value: number) => {
+    setValue(value);
+  };
+
   const updateValue = React.useCallback(() => {
     if (defaultValue < min || defaultValue > max) {
       console.warn(`Default value set to ${defaultValue} but it must be between ${min} and ${max}`);
@@ -43,13 +51,21 @@ const Slider: React.FC<Props> = (props) => {
 
   // Life-cycle
   React.useEffect(updateValue, [defaultValue]);
+  const { onDrag, onDragEnd, onDragStart } = useSlide(config, onDragUpdate, onChange);
 
   return (
     <div className={classes} style={style}>
-      <div>
+      <div ref={draggingArea}>
         <div className={classesBackground} />
         <div className={classesForeground} style={{ width: percentage }} />
-        <div className={styles.handle} style={{ left: handleLeft }} />
+        <div
+          className={styles.handle}
+          draggable={true}
+          onDrag={onDrag}
+          onDragEnd={onDragEnd}
+          onDragStart={onDragStart}
+          style={{ left: handleLeft }}
+        />
       </div>
     </div>
   );
---
+++ packages/slider/useSlide.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+
+const NO_IMAGE = new Image();
+NO_IMAGE.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+
+type DragEvent = React.DragEvent<HTMLDivElement>;
+
+type SlideConfig = {
+  draggingArea: React.RefObject<HTMLDivElement>;
+  max: number;
+  min: number;
+  value: number;
+};
+
+const useSlide = (
+  config: SlideConfig,
+  onUpdate: (value: number) => void,
+  onEnd?: (newValue: number, oldValue: number) => void
+) => {
+  const { draggingArea, max, min, value } = config;
+
+  // Hooks
+  const initialDragPosition = React.useRef<number | null>(null);
+  const initialValue = React.useRef<number | null>(null);
+
+  // Handlers
+  const onDragStart = React.useCallback(
+    (e: DragEvent) => {
+      initialDragPosition.current = e.clientX;
+      initialValue.current = value;
+      e.dataTransfer.setDragImage(NO_IMAGE, 0, 0);
+    },
+    [value]
+  );
+
+  const onDragEnd = React.useCallback((e: DragEvent) => {
+    onEnd?.(value, initialValue.current ?? 0);
+
+    initialDragPosition.current = null;
+    initialValue.current = null;
+  }, []);
+
+  const onDrag = React.useCallback(
+    (e: DragEvent) => {
+      if (e.screenX === 0) return;
+
+      if (!initialDragPosition.current || !draggingArea.current || !initialValue.current) return;
+
+      const offset = e.clientX - initialDragPosition.current;
+
+      const { width } = draggingArea.current.getBoundingClientRect();
+      const relativeMovement = (max - min) * (offset / width);
+
+      const newValue = Math.min(max, Math.max(min, initialValue.current + relativeMovement));
+
+      if (newValue != value) onUpdate(newValue);
+    },
+    [min, max, value]
+  );
+
+  return { onDrag, onDragEnd, onDragStart };
+};
+
+export { useSlide };
